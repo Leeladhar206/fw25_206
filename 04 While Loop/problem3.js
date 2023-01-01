fd20a299e5511726733b39877cf211a9f3bbc829
Leeladhar206 <leeladharjagarapu@gmail.com>
2023-01-01 22:40:06 +0530

    iw

---
+++ 04 While Loop/problem3.js
@@ -0,0 +1,7 @@
+let sum=0;
+for(let i=1;i<=20;i++){
+  if(i%3 ==0){
+    sum = i+sum
+  }
+}
+  console.log(sum)
---
+++ 04 While Loop/problem4.js
@@ -0,0 +1,9 @@
+let count=0;
+let sum=1;
+for(let i=1;i<=100;i++){
+  if(i%2==0){
+    sum= i+sum
+    count++
+  }
+}
+console.log(sum/count);
